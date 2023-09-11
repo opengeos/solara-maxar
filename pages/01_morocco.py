e0a347304c9d93c5d98ccc9f2e8e11199eb5c4f5
Qiusheng Wu <giswqs@gmail.com>
2023-09-11 16:00:50 -0400

    Update web app

---
+++ pages/01_morocco.py
@@ -0,0 +1,121 @@
+import os
+import leafmap
+import solara
+import ipyleaflet
+import ipywidgets as widgets
+import pandas as pd
+
+url = 'https://open.gishub.org/maxar-open-data'
+repo = 'https://github.com/opengeos/maxar-open-data/blob/master/datasets'
+
+
+def get_datasets():
+    datasets = f'{url}/datasets.csv'
+    df = pd.read_csv(datasets)
+    return df
+
+
+def get_catalogs(name):
+    dataset = f'{url}/datasets/{name}.tsv'
+
+    dataset_df = pd.read_csv(dataset, sep='\t')
+    catalog_ids = dataset_df['catalog_id'].unique().tolist()
+    catalog_ids.sort()
+    return catalog_ids
+
+
+def update_geojson(m):
+    datasets = get_datasets()['dataset'].tolist()
+
+    style = {"description_width": "initial"}
+    padding = "0px 0px 0px 5px"
+    dataset = widgets.Dropdown(
+        options=datasets,
+        description='Event:',
+        value="Morocco-Earthquake-Sept-2023",
+        style=style,
+        layout=widgets.Layout(width="270px", padding=padding),
+    )
+
+    image = widgets.Dropdown(
+        value=None,
+        options=get_catalogs(dataset.value),
+        description='Image:',
+        style=style,
+        layout=widgets.Layout(width="270px", padding=padding),
+    )
+
+    checkbox = widgets.Checkbox(
+        value=True,
+        description='Show footprints',
+        style=style,
+        layout=widgets.Layout(width="130px", padding=padding),
+    )
+
+    def change_dataset(change):
+        default_geojson = f'{url}/datasets/{change.new}.geojson'
+        m.layers = m.layers[:2]
+        m.controls = m.controls[:-1]
+        m.add_geojson(default_geojson, layer_name='Footprint', zoom_to_layer=True)
+        image.options = get_catalogs(change.new)
+
+    dataset.observe(change_dataset, names='value')
+
+    def change_image(change):
+        if change.new:
+            mosaic = f'{url}/datasets/{dataset.value}/{image.value}.json'
+            m.add_stac_layer(mosaic, name=image.value)
+
+    image.observe(change_image, names='value')
+
+    def change_footprint(change):
+        geojson_layer = m.find_layer('Footprint')
+        if change.new:
+            geojson_layer.visible = True
+        else:
+            geojson_layer.visible = False
+
+    checkbox.observe(change_footprint, names='value')
+
+    event_control = ipyleaflet.WidgetControl(widget=dataset, position='topright')
+    image_control = ipyleaflet.WidgetControl(widget=image, position='topright')
+    checkbox_control = ipyleaflet.WidgetControl(widget=checkbox, position='topright')
+    m.add(event_control)
+    m.add(image_control)
+    m.add(checkbox_control)
+
+
+zoom = solara.reactive(2)
+center = solara.reactive((20, 0))
+
+
+class Map(leafmap.Map):
+    def __init__(self, **kwargs):
+        kwargs['toolbar_control'] = False
+        super().__init__(**kwargs)
+        self.add_basemap('Esri.WorldImagery', show=False)
+        self.add_layer_manager(opened=False)
+        update_geojson(self)
+        default_geojson = f'{url}/datasets/Morocco-Earthquake-Sept-2023.geojson'
+        self.add_geojson(default_geojson, layer_name='Footprint', zoom_to_layer=True)
+
+
+@solara.component
+def Page():
+    with solara.Column(style={"min-width": "500px"}):
+        # solara components support reactive variables
+        # solara.SliderInt(label="Zoom level", value=zoom, min=1, max=20)
+        # using 3rd party widget library require wiring up the events manually
+        # using zoom.value and zoom.set
+        Map.element(  # type: ignore
+            zoom=zoom.value,
+            on_zoom=zoom.set,
+            center=center.value,
+            on_center=center.set,
+            scroll_wheel_zoom=True,
+            toolbar_ctrl=False,
+            data_ctrl=False,
+            height="780px",
+        )
+        # solara.Text(f"Zoom: {zoom.value}")
+        # solara.Text(f"Center: {center.value}")
